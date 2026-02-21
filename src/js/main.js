66601f5dc4c649b3f6807ed50b4e7943255a432c
ImuneGH <MartinSotnar93@gmail.com>
2026-02-21 20:40:52 +0100

    feat: přidána funkce na správný tvar slov v timeru

--- src/js/main.js
+++ src/js/main.js
@@ -8,15 +8,24 @@ function updateWeddingTimer() {
   const hourDifference = Math.floor((dateDifference / (1000 * 60 * 60)) % 24);
   const minuteDifference = Math.floor((dateDifference / (1000 * 60)) % 60);
   const secondsDifference = Math.floor((dateDifference / 1000) % 60);
-  const formattedRemainingTime = dayDifference + " dní " + hourDifference + " hodin " + minuteDifference + " minut " + secondsDifference + " sekund ";
+  const [days, hours, minutes, seconds] = correctWordFormat(dayDifference, hourDifference, minuteDifference, secondsDifference);
+  const formattedRemainingTime = dayDifference + days + hourDifference + hours + minuteDifference + minutes + secondsDifference + seconds;
   return formattedRemainingTime;
+}
+
+function correctWordFormat(days, hours, minutes, seconds) {
+  const formattedDays = days > 4 || days === 0 ? " dní " : days !== 1 ? " dny " : "den";
+  const formattedHours = hours > 4 || hours === 0 ? " hodin " : hours !== 1 ? " hodiny " : " hodina ";
+  const formattedMinutes = minutes > 4 || minutes === 0 ? " minut " : minutes !== 1 ? " minuty " : " minuta ";
+  const formattedSeconds = seconds > 4 || seconds === 0 ? " sekund " : seconds !== 1 ? " sekundy " : " sekunda ";
+  return [formattedDays, formattedHours, formattedMinutes, formattedSeconds];
 }
 
 // main code
 
 const timerElement = document.querySelector(".timer");
-timerElement.textContent = updateWeddingTimer();
+timerElement.textContent = "za " + updateWeddingTimer();
 
 setInterval(() => {
-  timerElement.textContent = updateWeddingTimer();
+  timerElement.textContent = "za " + updateWeddingTimer();
 }, 1000);
