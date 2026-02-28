f8ff7862c134afacf92cd466e666b8107380e230
ImuneGH <MartinSotnar93@gmail.com>
2026-02-28 15:36:30 +0100

    feat: upravena semantika faq sekce

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,14 +1,13 @@
 // functions
 
 function showAnswer(e) {
-  const answer = e.target.nextSibling;
+  const answer = e.target.nextElementSibling;
   answer.classList.toggle("display-none");
 }
 
 // main code
 
 const questionElements = document.querySelectorAll(".question");
-const answerElements = document.querySelectorAll(".answer");
 
 questionElements.forEach((question) => {
   question.addEventListener("click", showAnswer);
