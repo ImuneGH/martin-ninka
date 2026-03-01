0e430c1b02842fb1a2c9eff470930513427cf6f9
ImuneGH <MartinSotnar93@gmail.com>
2026-03-01 20:00:25 +0100

    feat: pilování faq animace

--- src/js/animations.js
+++ src/js/animations.js
@@ -4,12 +4,13 @@ import gsap from "gsap";
 
 function showAnswer(e) {
   const answer = e.target.nextElementSibling;
+  let isOpen = answer.classList.contains("is-open");
+  answer.classList.toggle("is-open");
+  console.log(isOpen);
   if (!isOpen) {
-    isOpen = true;
-    gsap.to(answer, { opacity: 1, duration: 0.5, height: "auto", padding: 16, border: "1px solid var(--accent-color-hover)" });
+    gsap.to(answer, { opacity: 1, duration: 0.3, height: "auto", padding: 16, border: "1px solid var(--accent-color-hover)" });
   } else {
-    isOpen = false;
-    gsap.to(answer, { opacity: 0, height: 0, duration: 0.5, padding: 0, border: "0px solid var(--accent-color-hover)" });
+    gsap.to(answer, { opacity: 0, height: 0, duration: 0.3, padding: 0, border: "0px solid var(--accent-color-hover)" });
   }
 }
 
@@ -17,7 +18,6 @@ function showAnswer(e) {
 
 // faq animations
 
-let isOpen = false;
 const questionElements = document.querySelectorAll(".question");
 
 questionElements.forEach((question) => {
