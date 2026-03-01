3443c860bf8ad476e6cf87d0883be9d54949982a
ImuneGH <MartinSotnar93@gmail.com>
2026-03-01 20:01:30 +0100

    chore: promazání logů

--- src/js/animations.js
+++ src/js/animations.js
@@ -6,7 +6,6 @@ function showAnswer(e) {
   const answer = e.target.nextElementSibling;
   let isOpen = answer.classList.contains("is-open");
   answer.classList.toggle("is-open");
-  console.log(isOpen);
   if (!isOpen) {
     gsap.to(answer, { opacity: 1, duration: 0.3, height: "auto", padding: 16, border: "1px solid var(--accent-color-hover)" });
   } else {
