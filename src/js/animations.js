7ffb8dd4858d373fb048a658ba931cb32228f9b3
ImuneGH <MartinSotnar93@gmail.com>
2026-03-06 20:07:21 +0100

    feat: arrow ve faq zprovozněn v JS

--- src/js/animations.js
+++ src/js/animations.js
@@ -4,12 +4,15 @@ import gsap from "gsap";
 
 function showAnswer(e) {
   const answer = e.target.nextElementSibling;
+  const arrow = e.target.firstElementChild;
   let isOpen = answer.classList.contains("is-open");
   answer.classList.toggle("is-open");
   if (!isOpen) {
     gsap.to(answer, { opacity: 1, duration: 0.3, height: "auto", padding: 16, border: "1px solid var(--accent-color-hover)" });
+    gsap.to(arrow, { rotate: 135, duration: 0.3 });
   } else {
     gsap.to(answer, { opacity: 0, height: 0, duration: 0.3, padding: 0, border: "0px solid var(--accent-color-hover)" });
+    gsap.to(arrow, { rotate: -45, duration: 0.3 });
   }
 }
 
